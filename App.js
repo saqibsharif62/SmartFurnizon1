5e129c2478c3e90ced1b1c9fc42b8fb965bb5e81
saqibsharif62 <70163424+saqibsharif62@users.noreply.github.com>
2020-10-27 16:43:39 +0500

    Update App.js

--- App.js
+++ App.js
@@ -17,6 +17,23 @@ import DrawerContent from './screen/DrawerContent';
 import ProductManagement from './screen/ProductManagement';
 import AddCategory from './screen/AddCategory';
 import RemoveCategory from './screen/RemoveCategory';
+import SBeds from './screen/SearchResults/Beds';
+import SChairs from './screen/SearchResults/Chairs';
+import SCouches from './screen/SearchResults/Couches';
+import SOfficeChair from './screen/SearchResults/OfficeChair';
+import STables from './screen/SearchResults/Tables';
+import CameraContainer from './screen/ImageRecognition/CameraContainer';
+import Details from './screen/Details/Details';
+import DLuxuryChair from './screen/Details/NewProducts/DLuxuryChair';
+import DComputerDesk from './screen/Details/NewProducts/DComputerDesk';
+import DCouch from './screen/Details/NewProducts/DCouch';
+import DDiningTable from './screen/Details/NewProducts/DDiningTable';
+import DOfficeTable from './screen/Details/NewProducts/DOfficeTable';
+import DBed1 from './screen/Details/Products/DBed1';
+import DChair1 from './screen/Details/Products/DChair1';
+import DTable1 from './screen/Details/Products/DTable1';
+import DCouch1 from './screen/Details/Products/DCouch1';
+import DOffChair1 from './screen/Details/Products/DOffChair1';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -53,7 +70,9 @@ class App extends Component {
           <Stack.Screen
             name="Home"
             component= {Home}
-            hideNavBar={true} />
+            options = {{
+              headerShown : false,
+            }} />
           <Stack.Screen
             name="AdminLogin"
             component= {AdminLogin}
@@ -88,6 +107,126 @@ class App extends Component {
             options = {{
             headerShown : false,
             }} />
+
+          <Stack.Screen
+            name="SBeds"
+            component= {SBeds}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="SChairs"
+            component= {SChairs}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="STables"
+            component= {STables}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="SCouches"
+            component= {SCouches}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="SOfficeChair"
+            component= {SOfficeChair}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="CameraContainer"
+            component= {CameraContainer}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="Details"
+            component= {Details}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="DLuxuryChair"
+            component= {DLuxuryChair}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="DCouch"
+            component= {DCouch}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="DComputerDesk"
+            component= {DComputerDesk}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="DDiningTable"
+            component= {DDiningTable}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="DOfficeTable"
+            component= {DOfficeTable}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="DBed1"
+            component= {DBed1}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="DChair1"
+            component= {DChair1}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="DTable1"
+            component= {DTable1}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="DCouch1"
+            component= {DCouch1}
+            options = {{
+            headerShown : false,
+            }} />
+
+          <Stack.Screen
+            name="DOffChair1"
+            component= {DOffChair1}
+            options = {{
+            headerShown : false,
+            }} />
+
         </Stack.Navigator>
       </NavigationContainer>
     );
